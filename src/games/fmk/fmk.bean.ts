c1ac0bcd1953ab64d59bf3daf9209a9a7e813488
LuisMayo <luismayovalbuena@outlook.es>
2020-05-03 13:34:06 +0200

    Removed unusued things

--- src/games/fmk/fmk.bean.ts
+++ src/games/fmk/fmk.bean.ts
@@ -1,6 +1,6 @@
 import { GameObject } from "../game.interface";
 import { Context } from "telegraf";
-import { MessageMedia, InputMediaPhoto } from "telegraf/typings/telegram-types";
+import { InputMediaPhoto } from "telegraf/typings/telegram-types";
 import { FMKGameService } from "./fmk-service";
 
 export class FMKBean implements GameObject {
@@ -8,12 +8,6 @@ export class FMKBean implements GameObject {
 
     sendMessage(ctx: Context): void {
         let media: InputMediaPhoto[] = [];
-        let testMessage = `Who would you fuck, marry and kill? (Hint: Open an image to get their name)\n1️⃣: F: ${this.people[0].name} M: ${this.people[1].name} K:${this.people[2].name}\n` +
-        `2️⃣: F: ${this.people[2].name} M: ${this.people[0].name} K:${this.people[1].name}\n` + 
-        `3️⃣: F: ${this.people[1].name} M: ${this.people[2].name} K:${this.people[0].name}\n` +
-        `4️⃣: F: ${this.people[2].name} M: ${this.people[1].name} K:${this.people[0].name}\n` +
-        `5️⃣: F: ${this.people[0].name} M: ${this.people[2].name} K:${this.people[1].name}\n` +
-        `6️⃣: F: ${this.people[1].name} M: ${this.people[0].name} K:${this.people[2].name}\n`;
         for (const person of this.people) {
             media.push({ media: person.image, type: 'photo', caption: person.name });
         }
